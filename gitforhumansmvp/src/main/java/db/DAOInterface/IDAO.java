fd515c95a19a573e67c645c316e589d135e79d22
Judacio500 <darkiman18@gmail.com>
2026-06-07 15:29:13 -0600

    ADDED: Generic DAO Interface

---
+++ gitforhumansmvp/src/main/java/db/DAOInterface/IDAO.java
@@ -0,0 +1,20 @@
+package db.DAOInterface;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IDAO<T, K> 
+{
+    // C - Create
+    boolean insert(T obj) throws SQLException;
+    
+    // R - Read
+    T selectById(K id) throws SQLException;
+    List<T> selectAll() throws SQLException;
+    
+    // U - Update
+    boolean update(T obj) throws SQLException;
+    
+    // D - Delete
+    boolean delete(K id) throws SQLException;
+}
