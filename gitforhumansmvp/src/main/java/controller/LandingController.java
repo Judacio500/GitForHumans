2411ea8b0ed83f6a515ffb3eda111c6f7a35beea
Judacio500 <darkiman18@gmail.com>
2026-06-08 15:49:55 -0600

    UPDATE: Changed LandingController Page path to be the root page for the WEB APP

--- gitforhumansmvp/src/main/java/controller/LandingController.java
+++ gitforhumansmvp/src/main/java/controller/LandingController.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/home")
+@WebServlet(urlPatterns = {"", "/home"})
 public class LandingController extends HttpServlet 
 {
     private static final long serialVersionUID = 1L;
